64f6494729ff8b0e1967364cf24442ad252a785e
Yuvethachinna <118183398+Yuvethachinna@users.noreply.github.com>
2022-11-18 14:04:34 +0530

    Print Number
    print number 1 to 10 and 10 to 1

---
+++ PrintNumber.java
@@ -0,0 +1,16 @@
+package com.chainsys.day3;
+
+public class PrintNumber {
+
+	public static void main(String[] args) {
+		System.out.println("1 to 10:");
+		for(int i=1;i<=10;i++) {
+		System.out.println(i);
+	}
+		System.out.println("10 to 1");
+		for(int j=10;j>=1;j--) {
+			System.out.println(j);
+		}
+		
+}
+}
