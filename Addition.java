0034106f7d80d2192b727c738d44c5c06ed8d382
Yuvethachinna <118183398+Yuvethachinna@users.noreply.github.com>
2022-11-18 14:10:07 +0530

    Addition
    Addition output

---
+++ Addition.java
@@ -0,0 +1,24 @@
+package com.chainsys.day3;
+
+public class Addition {
+
+	
+		
+		    public static void main(String s[])
+		    {
+		        int a = 4;
+		        a++;
+		        a += a;
+		        --a;
+		        a = 7 + a;
+		        a *= a;
+		        a -= 3;
+
+		        System.out.println(" a = " + a );
+		    }
+		
+		
+
+	}
+
+
