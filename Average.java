56419030d5a3b7dace7053d3111252aec607b4f9
Yuvethachinna <118183398+Yuvethachinna@users.noreply.github.com>
2022-11-18 14:13:49 +0530

    Average
    Average Number

---
+++ Average.java
@@ -0,0 +1,22 @@
+package com.chainsys.day3;
+import java.util.Scanner;
+public class Average {
+
+	public static void main(String[] args) {
+		int n, count = 1;   
+	      float  x, average, sum = 0;   
+	      Scanner sc = new Scanner(System.in);     
+	      System.out.println("Enter the value of n");  
+	      n = sc.nextInt();  
+	      while (count <= n)   
+	             {   
+	                  System.out.println("Enter the "+count+" number?");  
+	                  x = sc.nextInt();  
+	                  sum += x;   
+	                  ++count;   
+	             }   
+	                  average = sum/n;   
+	        System.out.println("The Average is"+average);  
+	}
+
+}
